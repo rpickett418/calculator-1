845d8eb462ca7e04e63998242803dbf7aa10386f
rpickett418 <86981398+rpickett418@users.noreply.github.com>
2021-07-12 14:05:47 -0600

    Fixed syntax errors

--- calc.js
+++ calc.js
@@ -13,7 +13,7 @@ reader.question("What would you like to calculate?", function(input){
 	mathSymbol = tokens[0];
 	num1 = Number(tokens[1]);
 	num2 = Number(tokens[2]);
-}
+
 	if (mathSymbol === "+"){
 		console.log(num1 + num2);
 	}
@@ -38,3 +38,4 @@ reader.question("What would you like to calculate?", function(input){
 
 	// This line closes the connection to the command line interface.
 	reader.close();
+})
